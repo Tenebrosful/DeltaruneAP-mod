04f4dcde68f862501bb57e9eb316b8b51ee83f82
Tenebrosful <tenebrosful@proton.me>
2026-04-19 05:34:35 +0200

    BSDIFF GEN

---
+++ generate_bsdiff.py
@@ -0,0 +1,25 @@
+import bsdiff4
+
+bsdiff4.file_diff(
+    "DELTARUNE/data_vanilla.win", "DELTARUNE/data.win", "deltarune.bsdiff"
+)
+bsdiff4.file_diff(
+    "DELTARUNE/chapter1_windows/data_vanilla.win",
+    "DELTARUNE/chapter1_windows/data.win",
+    "ch1.bsdiff",
+)
+# bsdiff4.file_diff(
+#     "DELTARUNE/chapter2_windows/data_vanilla.win",
+#     "DELTARUNE/chapter2_windows/data.win",
+#     "ch2.bsdiff",
+# )
+# bsdiff4.file_diff(
+#     "DELTARUNE/chapter3_windows/data_vanilla.win",
+#     "DELTARUNE/chapter3_windows/data.win",
+#     "ch3.bsdiff",
+# )
+# bsdiff4.file_diff(
+#     "DELTARUNE/chapter4_windows/data_vanilla.win",
+#     "DELTARUNE/chapter4_windows/data.win",
+#     "ch4.bsdiff",
+# )
