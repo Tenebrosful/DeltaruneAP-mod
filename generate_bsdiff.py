dfad0ff287a3fcb8ed0856bb059ca9a96d8b480a
Tenebrosful <tenebrosful@proton.me>
2026-05-06 00:48:20 +0200

    enable ch3 bsdiff

--- generate_bsdiff.py
+++ generate_bsdiff.py
@@ -13,11 +13,11 @@ bsdiff4.file_diff(
     "DELTARUNE/chapter2_windows/data.win",
     "ch2.bsdiff",
 )
-# bsdiff4.file_diff(
-#     "DELTARUNE/chapter3_windows/data_vanilla.win",
-#     "DELTARUNE/chapter3_windows/data.win",
-#     "ch3.bsdiff",
-# )
+bsdiff4.file_diff(
+    "DELTARUNE/chapter3_windows/data_vanilla.win",
+    "DELTARUNE/chapter3_windows/data.win",
+    "ch3.bsdiff",
+)
 # bsdiff4.file_diff(
 #     "DELTARUNE/chapter4_windows/data_vanilla.win",
 #     "DELTARUNE/chapter4_windows/data.win",
